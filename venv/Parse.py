5f7054896345951163b9000a746e558933493299
mrberry <sashinn@ucsd.edu>
2019-06-19 22:31:31 -0700

    Fixed readFile error

--- venv/Parse.py
+++ venv/Parse.py
@@ -8,29 +8,27 @@ __status__ = "Prototype"
 
 
 class Parse:
-    #filename = "2019613_.lvm"
 
     def __init__(self, filename):
         self.filename = filename
 
     def getDate(self):
-        return "6/19/2019"
+        return "2019-06-19"
 
-    def _read(filename):
+    def _readFile(self, filename):
         file = open(filename, "r")
         data = file.read()
         return data
 
     def getData(self):
-        data = _read(self.filename)
+        data = self._readFile(self.filename)
         return data
 
     def printVar(self):
         print(self.filename)
 
+
 obj = Parse("2019613_.lvm")
 obj.printVar()
 print(obj.getDate())
 print(obj.getData())
-
-
