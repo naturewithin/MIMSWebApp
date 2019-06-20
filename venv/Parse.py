b92344165be48ce4b5d00681d64e319c29dc530c
retirotigre <sashinn@ucsd.edu>
2019-06-19 20:21:12 -0700

    started parse, learning python class syntax

--- venv/Parse.py
+++ venv/Parse.py
@@ -6,9 +6,31 @@ __author__ = "Sarah Shinn"
 __email__ = "sashinn@ucsd.edu"
 __status__ = "Prototype"
 
-#test
-file1 = open("2019613_.lvm", "r")
-line = file1.read()
-print(line)
+
+class Parse:
+    #filename = "2019613_.lvm"
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def getDate(self):
+        return "6/19/2019"
+
+    def _read(filename):
+        file = open(filename, "r")
+        data = file.read()
+        return data
+
+    def getData(self):
+        data = _read(self.filename)
+        return data
+
+    def printVar(self):
+        print(self.filename)
+
+obj = Parse("2019613_.lvm")
+obj.printVar()
+print(obj.getDate())
+print(obj.getData())
 
 
