62575935ce86a424dd6ea3946d0a151405eacf8c
mrberry <sashinn@ucsd.edu>
2019-06-20 17:18:40 -0700

    cleaned code

--- venv/Parse.py
+++ venv/Parse.py
@@ -10,15 +10,15 @@ __status__ = "Prototype"
 # the Parse class opens a .lvm file and creates a 2-dimensional array of values
 class Parse:
 
-    # fetch .lvm file by "Last Modified" and initialize a variable filename
+    # TODO: fetch .lvm file by "Last Modified" and initialize filename
     def __init__(self, filename):
         self.filename = filename
-        # pass instance variable filename into constructor
+        # TODO: pass instance variable filename into constructor
 
     def get_date(self):
         return "2019-06-19"
 
-    # research how to add a try catch block and how to control errors here
+    # TODO: research how to add a try catch block and how to control errors here
     def _read_file(self):
         with open(self.filename, "r") as f:
             data = f.read()
@@ -35,12 +35,12 @@ class Parse:
         start_index = data.find(word) + len(word)
         if start_index != -1:
             data_block = data[start_index:]
-            grid = data_block.split('\n') # create grid rows
-            grid.pop(0) # remove first blank value
-            grid.pop(len(grid) - 1) # remove last blank value
+            grid = data_block.split('\n')  # create grid rows
+            grid.pop(0)  # remove first blank value
+            grid.pop(len(grid) - 1)  # remove last blank value
             col = 0
             for row in grid:
-                grid[col] = row.split('\t') # create grid columns
-                grid[col].pop(0) # remove leading blank values
+                grid[col] = row.split('\t')  # create grid columns
+                grid[col].pop(0)  # remove leading blank values
                 col += 1
         return grid
